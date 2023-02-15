c38208ff437d6ef772d04ab2323b9fbc87a3cd10
Pouyan <pouyannc@gmail.com>
2023-02-15 17:44:05 -0500

    Menu and contacts page

--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,32 @@
+import phoneImg from './images/call.png';
+import addressImg from './images/map-pin.png';
+
 const contactDiv = () => {
   const contact = document.createElement('div');
   contact.className = 'contact';
+
+  const phoneDiv = document.createElement('div');
+  phoneDiv.className = 'phone';
+  const phoneIcon = document.createElement('img');
+  phoneIcon.src = phoneImg;
+  const phoneNumber = document.createElement('div');
+  phoneNumber.innerHTML = '(416)-222-3333';
+
+  phoneDiv.appendChild(phoneIcon);
+  phoneDiv.appendChild(phoneNumber);
+
+  const addressDiv = document.createElement('div');
+  addressDiv.className = 'address';
+  const addressIcon = document.createElement('img');
+  addressIcon.src = addressImg;
+  const address = document.createElement('div');
+  address.innerHTML = '111 Cloudy St. W.';
+
+  addressDiv.appendChild(addressIcon);
+  addressDiv.appendChild(address);
+
+  contact.appendChild(phoneDiv);
+  contact.appendChild(addressDiv);
 
   return contact;
 };
--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,89 @@
+import mistyCoffee from './images/latte-gd824188dc_1920.jpg';
+import blackCoffee from './images/coffee-gd5daf92ac_1920.jpg';
+import americanoImg from './images/coffee-g5f78487b6_1920.jpg';
+import latteImg from './images/coffee-gca9f266ee_1920.jpg';
+import cappImg from './images/dan-smedley-765Ogj6zVuI-unsplash.jpg';
+import espressoImg from './images/adi-goldstein-xKS-1DP4g7A-unsplash.jpg';
+import earlImg from './images/yana-bhKdRjYJ1CA-unsplash.jpg';
+import pekoeImg from './images/lucas-george-wendt-AbfVHFSEIKk-unsplash.jpg';
+import matchaImg from './images/zoe-N4GN-4s9-rs-unsplash.jpg';
+import peachHerbalImg from './images/teacora-rooibos-sOp_AVUXk98-unsplash.jpg';
+import gingerHerbalImg from './images/julia-topp-kuIiEew8V-A-unsplash.jpg';
+
 const menuDiv = () => {
   const menu = document.createElement('div');
   menu.className = 'menu';
+
+  const coffee = document.createElement('div');
+  coffee.className = 'coffee';
+  const coffeeTitle = document.createElement('div');
+  coffeeTitle.className = 'coffee-title';
+  coffeeTitle.innerHTML = 'Coffees';
+
+  coffee.appendChild(coffeeTitle);
+
+  const coffeeMenu = document.createElement('div');
+  coffeeMenu.className = 'coffee-menu';
+  coffee.appendChild(coffeeMenu);
+
+  const coffeeIds = ['misty-coffee', 'black-coffee', 'americano', 'latte', 'cappuccino', 'espresso'];
+  const coffeeNames = ['Misty Café au Lait', 'Coffee (Light/ Medium/ Dark Roast)', 'Americano', 'Latte', 'Cappuccino', 'Espresso'];
+  const coffeePrices = ['$3', '$2', '$3', '$4', '$4', '$2'];
+  const coffeeImages = [mistyCoffee, blackCoffee, americanoImg, latteImg, cappImg, espressoImg];
+
+  for (let i = 0; i < coffeeIds.length; i += 1) {
+    const coffeeItem = document.createElement('div');
+    coffeeItem.id = coffeeIds[i];
+
+    coffeeItem.appendChild(
+      Object.assign(document.createElement('img'), { className: 'coffee-image', src: coffeeImages[i] }),
+    );
+    coffeeItem.appendChild(
+      Object.assign(document.createElement('div'), { className: 'coffee-name', innerHTML: coffeeNames[i] }),
+    );
+    coffeeItem.appendChild(
+      Object.assign(document.createElement('div'), { className: 'coffee-price', innerHTML: coffeePrices[i] }),
+    );
+
+    coffeeMenu.appendChild(coffeeItem);
+  }
+
+  const tea = document.createElement('div');
+  tea.className = 'tea';
+  const teaTitle = document.createElement('div');
+  teaTitle.className = 'tea-title';
+  teaTitle.innerHTML = 'Teas';
+
+  tea.appendChild(teaTitle);
+
+  const teaMenu = document.createElement('div');
+  teaMenu.className = 'tea-menu';
+  tea.appendChild(teaMenu);
+
+  const teaIds = ['earl-grey', 'orange-pekoe', 'matcha', 'peach', 'ginger'];
+  const teaNames = ['Earl Grey', 'Orange Pekoe', 'Matcha Green Tea', 'Peach Herbal Tea', 'Ginger Herbal Tea'];
+  const teaPrices = ['$2', '$2', '$2', '$2', '$2'];
+  const teaImages = [earlImg, pekoeImg, matchaImg, peachHerbalImg, gingerHerbalImg];
+
+  for (let i = 0; i < teaIds.length; i += 1) {
+    const teaItem = document.createElement('div');
+    teaItem.id = teaIds[i];
+
+    teaItem.appendChild(
+      Object.assign(document.createElement('img'), { className: 'tea-image', src: teaImages[i] }),
+    );
+    teaItem.appendChild(
+      Object.assign(document.createElement('div'), { className: 'tea-name', innerHTML: teaNames[i] }),
+    );
+    teaItem.appendChild(
+      Object.assign(document.createElement('div'), { className: 'tea-price', innerHTML: teaPrices[i] }),
+    );
+
+    teaMenu.appendChild(teaItem);
+  }
+
+  menu.appendChild(coffee);
+  menu.appendChild(tea);
 
   return menu;
 };
